6d54f0c323c4f823d291b94e8195acbcba16bc94
akzubarev <alexkzubarev@gmail.com>
2025-01-07 17:12:01 +0400

    Added application export

--- backend/apps/games/admin/answer.py
+++ backend/apps/games/admin/answer.py
@@ -9,9 +9,15 @@ class AnswerAdmin(admin.ModelAdmin):
     """Answer admin."""
 
     ordering = ('-id',)
+    list_filter = ['application__user__username']
     list_display = [
-        "id",
-        "question",
-        "application__user",
-        "value",
+        'id',
+        'question',
+        'user_str',
+        'value',
     ]
+
+    @admin.display
+    def user_str(self, answer: Answer) -> str:  # type: ignore
+        user = answer.application.user
+        return f'{user.username} ({user.first_name} {user.last_name})'
