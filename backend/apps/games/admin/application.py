391d00e7cf072a97cc06833c16fa03a499b238cc
akzubarev <alexkzubarev@gmail.com>
2025-01-07 17:12:01 +0400

    Added application export

--- backend/apps/games/admin/application.py
+++ backend/apps/games/admin/application.py
@@ -1,19 +1,23 @@
 """Application admin module."""
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import path
 from django.utils.html import format_html
 
 from apps.games.models import Application
+from scripts.export_applications import export_apps
 
 
 @admin.register(Application)
 class ApplicationAdmin(admin.ModelAdmin):
     """Application admin."""
 
+    change_list_template = "games/applications_changelist.html"
     ordering = ('-id',)
     list_filter = ['game', 'user__username']
     list_display = [
         'id',
-        'user',
+        'user_str',
         'price',
         'payed',
         'game',
@@ -24,3 +28,19 @@ class ApplicationAdmin(admin.ModelAdmin):
 
     def likes(self, application: Application) -> str:
         return format_html('<br>'.join([str(characters) for characters in application.user.likes.all()]))
+
+    @admin.display
+    def user_str(self, application: Application) -> str:
+        user = application.user
+        return f'{user.username} ({user.first_name} {user.last_name})'
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('export/', self.export),
+        ]
+        return my_urls + urls
+
+    def export(self, request):
+        export_apps()
+        return HttpResponseRedirect("../")
---
+++ backend/scripts/export_applications.py
@@ -0,0 +1,100 @@
+"""Script for uploading characters from google sheet."""
+import json
+import os
+import sys
+from logging import getLogger
+from typing import Collection
+
+import django
+import gspread
+from gspread import Client, Worksheet, service_account_from_dict
+from gspread.utils import ValueInputOption
+
+if __name__ == '__main__':
+    sys.path[0] = '/app/'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    django.setup()
+
+from apps.games.models import Answer, Application, Question
+
+logger = getLogger(__name__)
+games_data = {
+    'spreadsheet_id': '1obEoiPrtLyIpVdWVODDfNz1gBeQ6EiDFRel82j_e584',
+    'whales': {'applications_worksheet_id': '418879773'},
+    # 'spreadsheet_id': '1ucdOv6Hid9toTFUSuTSNRh7z7wpeY2dgCkIOuZtgrz4',
+    # 'whales': {'applications_worksheet_id': '1417613417'},
+}
+
+
+def _get_google_credentials() -> Client:
+    """Получаем аккаунт для взаимодействия с гугл-таблицами."""
+    return service_account_from_dict(info=json.loads(os.getenv('GOOGLE_CREDS')))
+
+
+def _get_worksheet(spreadsheet_id: str, worksheet_id: str) -> gspread.Worksheet:
+    """Get a worksheet from Google Spreadsheet."""
+    google_creds = _get_google_credentials()
+    return google_creds.open_by_key(key=spreadsheet_id).get_worksheet_by_id(id=worksheet_id)
+
+
+def _get_answer_value(answer: Answer | None) -> list[str]:
+    if not answer:
+        return ['']
+    value = answer.value
+    if isinstance(value, list):
+        if isinstance(value[0], list):
+            return [','.join(v) for v in value]
+        return [','.join(value)]
+    return [value]
+
+
+def get_titles(question: Question) -> list:
+    if isinstance(question.choices, list) and len(question.choices) == 2 and isinstance(question.choices[1], list):
+        return [f'{question.title} [{choice}]' for choice in question.choices[1]]
+    return [question.title]
+
+
+def _make_title(questions: Collection[Question]):
+    title = ['Кто']
+    for question in questions:
+        title.extend(get_titles(question=question))
+    return title
+
+
+def upload_applications(worksheet: Worksheet, game_alias: str) -> None:
+    """Формирование и выгрузка в гугл таблицу рядов данных рекламных кампаний."""
+    questions = Question.objects.filter(games__alias=game_alias).order_by('order')
+    applications = Application.objects.filter(game__alias=game_alias).order_by('id')
+    rows = [_make_title(questions=questions)]
+    for application in applications:
+        answers = {answer.question_id: answer for answer in application.answers.all()}
+        answers_values = []
+        for question in questions:
+            answers_values.extend(_get_answer_value(answers.get(question.id)))
+        if any(answers_values):
+            user = application.user
+            rows.append([f'{user.username} ({user.first_name} {user.last_name})'] + answers_values)
+    if rows:
+        worksheet.clear()
+        worksheet.append_rows(
+            values=rows, value_input_option=ValueInputOption.user_entered,
+        )
+        logger.info(f'Выгружено {len(applications)} заявок.')
+
+
+def export_apps():
+    """Entrypoint for loading games characters."""
+    for game_alias in [
+        # 'frostpunk',
+        'whales'
+    ]:
+        upload_applications(
+            game_alias=game_alias, worksheet=_get_worksheet(
+                spreadsheet_id=games_data['spreadsheet_id'],
+                worksheet_id=games_data[game_alias]['applications_worksheet_id'],
+            )
+        )
+
+
+if __name__ == '__main__':
+    export_apps()
